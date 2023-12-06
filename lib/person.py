6fd1e240a46258298555655b194cb470a9046a8e
Elisha <elishakibet67@gmail.com>
2023-12-06 13:11:38 +0300

    Update person.py

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,20 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self):
+        self._job = None
+    
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, job_type):
+        if job_type not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job_type
+
+employee1 = Person()
+employee1.job = "Marketing"
+print(employee1.job)
